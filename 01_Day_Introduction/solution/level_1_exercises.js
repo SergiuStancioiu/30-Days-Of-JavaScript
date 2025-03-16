b5e19b792700f1f072adf8b8b16bbbd6b6677add
Sergiu Stancioiu <s.sergiu96@gmail.com>
2025-03-16 13:34:49 +0200

    feat: solution for day1

--- 01_Day_Introduction/solution/level_1_exercises.js
+++ 01_Day_Introduction/solution/level_1_exercises.js
@@ -1,15 +1,48 @@
 // 1. Write a single line comment which says, _comments can make code readable_
+// comments can make code readable
 
-/*
-2. Write another single comment which says, _Welcome to 30DaysOfJavaScript_
-3. Write a multiline comment which says, _comments can make code readable, easy to reuse_
-   _and informative_
+// 2. Write another single comment which says, _Welcome to 30DaysOfJavaScript_
+// Welcome to 30DaysOfJavaScript
 
-4. Create a variable.js file and declare variables and assign string, boolean, undefined and null data types
-5. Create datatypes.js file and use the JavaScript **_typeof_** operator to check different data types. Check the data type of each variable
-6. Declare four variables without assigning values
-7. Declare four variables with assigned values
-8. Declare variables to store your first name, last name, marital status, country and age in multiple lines
-9. Declare variables to store your first name, last name, marital status, country and age in a single line
-10. Declare two variables _myAge_ and _yourAge_ and assign them initial values and log to the browser console.
-*/
+//3. Write a multiline comment which says, _comments can make code readable, easy to reuse__and informative_
+/* comments can make code readable, easy to reuse */
+
+//4. Create a variable.js file and declare variables and assign string, boolean, undefined and null data types
+const stringVariable = '';
+const booleanVariable = false;
+const undefinedVariable = undefined;
+const nullVariable = null;
+
+//5. Create datatypes.js file and use the JavaScript **_typeof_** operator to check different data types. Check the data type of each variable
+
+typeof stringVariable;
+typeof booleanVariable;
+typeof undefinedVariable;
+typeof nullVariable;
+
+//6. Declare four variables without assigning values
+let var1;
+let var2;
+let var3;
+let var4;
+
+//7. Declare four variables with assigned values
+const var5 = 'Learn JavaScript';
+const var6 = true;
+const var7 = ['Learn', 'JavaScript'];
+const var8 = { 0: 'Learn', 1: 'JavaScript' };
+
+//8. Declare variables to store your first name, last name, marital status, country and age in multiple lines
+const firstName = 'Sergiu';
+const lastName = 'Stancioiu';
+const martialStatus = 'In a relationship';
+const country = 'Romania';
+const age = 28;
+
+//9. Declare variables to store your first name, last name, marital status, country and age in a single line
+let firstName2, lastName2, martialStatus2, country2, age2;
+
+//10. Declare two variables _myAge_ and _yourAge_ and assign them initial values and log to the browser console.
+const myAge = 28;
+const yourAge = 30;
+console.log(`My age is ${myAge} and your age is ${yourAge}`);
