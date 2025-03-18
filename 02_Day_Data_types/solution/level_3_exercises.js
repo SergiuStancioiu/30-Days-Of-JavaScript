939886a345ec3b0000b1200ec32a31a96627202a
Sergiu Stancioiu <s.sergiu96@gmail.com>
2025-03-18 19:45:49 +0200

    fix: attempting to fix issues mention on last review

--- 02_Day_Data_types/solution/level_3_exercises.js
+++ 02_Day_Data_types/solution/level_3_exercises.js
@@ -13,8 +13,16 @@ const nrLove = mySentenceArr.reduce((accumulator, word) => {
 // 2. Use __match()__ to count the number of all __because__ in the following sentence:__'You cannot end a sentence with because because because is a conjunction'__
 const sentence2 =
   'You cannot end a sentence with because because because is a conjunction';
+const sentence2Arr = sentence2.split(' ');
+const becauseArr = [];
 
-const nrOfBecause = [...sentence2.matchAll('because')].length;
+sentence2Arr.map((word) => {
+  if (word.match('because')) {
+    becauseArr.push(word);
+  }
+});
+
+becauseArr.length;
 
 /*
 3. Clean the following text and find the most frequent word (hint, use replace and regular expressions).
@@ -26,7 +34,33 @@ const nrOfBecause = [...sentence2.matchAll('because')].length;
 const sentence3 =
   '%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching';
 
-const sentenceWithoutSpecialCharacters = sentence3.replace(/[^\w\s]/gi, '');
+let sentWSC = sentence3.replace(/[^\w\s]/gi, '');
+
+function commonWord() {
+  if (sentWSC.length === 0) {
+    return null;
+  }
+
+  sentWSC = sentWSC.toLowerCase();
+  let commonWordCount = 0;
+  let commonWord = [];
+  sentWSC = sentWSC.split(' ');
+  sentWSC.forEach((word) => {
+    let wordValue = sentWSC.filter((w) => w === word).length;
+
+    if (wordValue > commonWordCount) {
+      commonWordCount = wordValue;
+      commonWord.length = 0;
+      commonWord.push(word);
+    } else if (wordValue == commonWordCount) {
+      commonWord.push(word);
+    }
+  });
+
+  console.log(commonWord);
+}
+
+commonWord();
 
 // 4. Calculate the total annual income of the person by extracting the numbers from the following text. 'He earns 5000 euro from salary per month, 10000 euro annual bonus, 15000 euro online courses per month.'
 const sentence4 =
