cfe892ebf6887ef0eae2355d0e2731bd8e398994
Sergiu Stancioiu <s.sergiu96@gmail.com>
2025-03-25 19:15:30 +0200

    fix: completed  exercise 3 from level_3_exercises

--- 02_Day_Data_types/solution/level_3_exercises.js
+++ 02_Day_Data_types/solution/level_3_exercises.js
@@ -20,9 +20,18 @@ const nrOfBecause = sentence2.match(/because/gi).length;
 const sentence3 =
   '%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching';
 
-let sentWSC = sentence3.replace(/[^\w\s]/gi, '');
+let cleanedText = sentence3.replace(/[^\w\s]/gi, '');
 
-console.log('sentWSC', sentWSC);
+const words = cleanedText.toLowerCase().split(/\s+/);
+
+const wordCount = {};
+words.forEach((word) => {
+  wordCount[word] = (wordCount[word] || 0) + 1;
+});
+
+const mostFrequentWord = Object.keys(wordCount).reduce((a, b) =>
+  wordCount[a] > wordCount[b] ? a : b
+); // teaching
 
 // 4. Calculate the total annual income of the person by extracting the numbers from the following text. 'He earns 5000 euro from salary per month, 10000 euro annual bonus, 15000 euro online courses per month.'
 const sentence4 =
